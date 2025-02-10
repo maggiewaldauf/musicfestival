2f38588e6635069fbfa9d0b41b4c0f3a971ef26a
maggiewaldauf <eaa24mwa@students.eaaa.dk>
2025-02-10 16:29:35 +0100

    Version2

--- src/views/PerformanceDetailPage.jsx
+++ src/views/PerformanceDetailPage.jsx
@@ -3,7 +3,9 @@ import { useParams } from "react-router-dom";
 export default function PerformanceDetailPage() {
     const eventdetail = [
         {uid: "1", time: "Coldplay will play at 10 am"}, 
-        {uid: "2", time: "Queen will play at 12 noon without Freddie, sorry!"}
+        {uid: "2", time: "Queen will play at 12 noon without Freddie, sorry!"},
+        {uid: "3", time: "Billie Eilish will play at 3 pm"}, 
+        {uid: "4", time: "Lady Gaga will play at 5pm"}
     ];
 
     const {eventId}= useParams();
